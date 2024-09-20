2df21c9b506f93fe5642ea9fc98c97adb798d307
DESKTOP-CSBAJF8\WLOPEZOB <wlopezob@gmail.com>
2024-09-19 19:57:21 -0500

    first commit

---
+++ src/main/java/com/wlopezob/myapi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.wlopezob.myapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+  @GetMapping
+  public Mono<String> hello() {
+    return Mono.just("hello world");
+  }
+
+}
---
+++ src/test/java/com/wlopezob/myapi/MyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlopezob.myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
